91da3b06b7c46f899411afd7ae5c7e17ea3a6dbb
D Ruth Bavousett <ruth@bywatersolutions.com>
2011-07-14 13:55:04 -0400

    Added new file

---
+++ migration/Generic/stock_userid_setup.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+#---------------------------------
+# Copyright 2011 ByWater Solutions
+#
+#---------------------------------
+#
+# -D Ruth Bavousett
+#
+#---------------------------------
+
+use strict;
+use warnings;
+use Getopt::Long;
+use Text::CSV;
+use C4::Context;
+use C4::Members;
+$|=1;
+my $debug=0;
+my $doo_eet=0;
+
+my $branch = "";
+GetOptions(
+    'branch:s'   => \$branch,
+    'update'     => \$doo_eet,
+    'debug'      => \$debug,
+);
+
+#if (($infile_name eq '')){
+#   print "You're missing something.\n";
+#   exit;
+#}
+
+my $dbh=C4::Context->dbh();
+my $i=0;
+my $query = "SELECT borrowernumber,cardnumber,userid,branchcode FROM borrowers";
+if ($branch ne q{}){
+   $query .= " WHERE branchcode = '$branch'";
+}
+
+my $find = $dbh->prepare($query);
+$find->execute();
+while (my $row=$find->fetchrow_hashref()){
+   $i++;
+   print ".";
+   print "\r$i" unless ($i % 100);
+   $debug and print "Changing $row->{'cardnumber'} ($row->{'branchcode'}) \n";
+   $doo_eet and C4::Members::ModMember(borrowernumber => $row->{'borrowernumber'}, 
+                                       userid         => $row->{'cardnumber'});
+}
+
+print "\n$i records updated.\n";
